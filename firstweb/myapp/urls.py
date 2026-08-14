1334699e03b8ca46d5a93c82d66f7f968bcaebc3
benzkubpom <benz_kubpom@hotmail.com>
2026-08-14 18:32:11 +0700

    Update project files

--- firstweb/myapp/urls.py
+++ firstweb/myapp/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import Home, About
 
 urlpatterns = [
-    path('', Home), #localhost:8000
-    path('about/', About), # localhost:8000/about
+    path('', Home, name='home'), #localhost:8000
+    path('about/', About, name='about'), # localhost:8000/about
 ]
